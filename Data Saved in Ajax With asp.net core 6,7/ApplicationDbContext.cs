8ed7d3682b7bce4f6f17c0aa150cb958e62ed776
Md.Ismail Hossain Miraj <123262611+mirajit@users.noreply.github.com>
2023-02-19 15:37:29 +0600

    Add files via upload

---
+++ Data Saved in Ajax With asp.net core 6,7/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebEmployee.Models;
+
+namespace WebEmployee.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<EmployeeModel> Employee { get; set; }
+        public virtual DbSet<Jsondatamodel> Jasondata { get; set; }
+
+    }
+}
---
+++ Data Saved in Ajax With asp.net core 6,7/EmployeeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using WebEmployee.Data;
+using WebEmployee.Helper;
+using WebEmployee.Models;
+
+namespace WebEmployee.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Jsonjdata()
+        {
+            try
+            {
+               
+                return View();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
+        }
+        [HttpPost]
+        public JsonResult Jsonjdata(string name,string mobile,string address)
+        {
+            try
+            {
+                Jsondatamodel jsondatamodel= new Jsondatamodel
+                {
+                    Name = name,
+                    Mobile = mobile,                        
+                    Address = address
+                };
+               _context.Add(jsondatamodel);
+                _context.SaveChanges();
+                return Json("Success");
+            }
+            catch (Exception ex)
+            {
+
+              throw new Exception(ex.Message);
+            }
+
+        }
+    }
+}
